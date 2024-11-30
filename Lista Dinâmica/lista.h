44afb279ab3e65ec6f8d5c70a02747a73e82dd47
Kzrtt <99203878+Kzrtt@users.noreply.github.com>
2024-11-30 01:07:21 -0300

    [fix]: removendo código inutil e adicionando alguns comentários

--- Lista Dinâmica/lista.h
+++ Lista Dinâmica/lista.h
@@ -27,6 +27,7 @@ void mostrar(LISTA** primeiro) {
 }
 
 LISTA* buscar(LISTA** primeiro, int pos) {
+    //? Se a lista está vázia retorna NULL
     if(*primeiro == NULL) {
         return NULL;
     }
@@ -43,8 +44,6 @@ LISTA* buscar(LISTA** primeiro, int pos) {
     return NULL;
 }
 
-//? Tipos de Inserção
-//? Começo / Meio / Fim
 void inserir(LISTA** primeiro, LISTA** ultimo, int* quantidade) {
     int escolha = 0;
     int auxPos = 0;
@@ -70,7 +69,7 @@ void inserir(LISTA** primeiro, LISTA** ultimo, int* quantidade) {
             break;
         case 2:
             printf("\nInserindo no meio...");
-            auxPos = *quantidade % 2 == 0 ? (*quantidade / 2) : (*quantidade / 2) + 1;
+            auxPos = *quantidade % 2 == 0 ? (*quantidade / 2) : (*quantidade / 2) - 1;
             break;
         default:
             printf("\nInserindo no fim...");
@@ -88,6 +87,9 @@ void inserir(LISTA** primeiro, LISTA** ultimo, int* quantidade) {
     printf("\nQual o valor: \n");
     scanf("%d", &novoNo->num);
 
+    //? Caso a inserção seja no fim ou no meio com menos de dois elementos:
+    //? O proxNo do Ultimo aponta para o novoNo
+    //? O Ultimo aponta para o novoNo
     if(escolha > 2 || (escolha == 2 && *quantidade < 2)) {
         (*ultimo)->proxNo = novoNo;
         *ultimo = novoNo;
@@ -97,13 +99,14 @@ void inserir(LISTA** primeiro, LISTA** ultimo, int* quantidade) {
         return;
     }
 
-    //? Buscando o valor anterior
+    //? Buscando valor na lista
     LISTA* temp = buscar(primeiro, auxPos);
     if(temp == NULL) {
-        //? Caso o anterior seja NULL a lista está vázia
+        //? Caso o anterior seja NULL a lista está vázia ou sendo inserido no primeiro lugar
         novoNo->proxNo = *primeiro;
         *primeiro = novoNo;
 
+        //? Caso o ultimo seja NULL ele recebe o novoNo
         if(*ultimo == NULL) {
             *ultimo = novoNo;
         }
@@ -112,14 +115,9 @@ void inserir(LISTA** primeiro, LISTA** ultimo, int* quantidade) {
         return;
     }
 
-    //? Se não for NULL processo normal de inserção
+    //? Se não for NULL significa que o valor está sendo inserido no meio
     novoNo->proxNo = temp->proxNo;
     temp->proxNo = novoNo;
-
-    if(escolha == 1) {
-        printf("teste");
-        *primeiro = novoNo;
-    }
 
     novoNo = NULL;
     free(novoNo);
@@ -147,16 +145,22 @@ void remover(LISTA** primeiro, LISTA** ultimo, int* quantidade) {
     LISTA* auxRem = NULL;
 
     if(remPos == 1) {
+        //? Se a remoção for do primeiro o auxRem recebe o primeiro
+        //? e o primeiro recebe o proxNo do primeiro
         auxRem = *primeiro;
         *primeiro = (*primeiro)->proxNo;
+        //? Se o primeiro for NULL significa que a lista está vázia então o ultimo também fica NULL
         if(*primeiro == NULL) {
             *ultimo = NULL;
         }
     } else {
+        //? Caso a remoção seja no meio ou fim o valor é buscado sendo pegado o valor anterior
         LISTA* temp = buscar(primeiro, remPos - 1);
+        //? auxRem recebe o proxNo (o que queremos remover) e o proxNo se torma o próximo do auxRem
         auxRem = temp->proxNo;
         temp->proxNo = auxRem->proxNo;
 
+        //? Se o que for removido for igual ao ultimo, o ultimo recebe o temp (novo ultimo)
         if(auxRem == *ultimo) {
             *ultimo = temp;
         }
